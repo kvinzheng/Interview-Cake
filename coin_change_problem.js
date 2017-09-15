05c2e6f8a25cf83043bd5ee374bc96a3a020b5ca
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-09-15 12:50:46 -0700

    updated coin change amount

---
+++ coin_change_problem.js
@@ -0,0 +1,14 @@
+// computes the number of ways to make the amount of money with coins of the available denominations.
+//
+// Example: for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢), your program would output 44—the number of ways to make 44¢ with those denominations:
+//
+// 1. 1¢, 1¢, 1¢, 1¢
+// 2. 1¢, 1¢, 2¢
+// 3. 1¢, 3¢
+// 4. 2¢, 2¢
+
+function changePossibilitiesTopDown(amountLeft, denominations, currentIndex) {
+  if(amountLeft === 0) {
+    
+  }
+}
