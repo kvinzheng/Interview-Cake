0ffe6fa61a4dc8dd3958abc05875919b43e444e2
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-09-18 10:25:56 -0700

    updated coin change

--- coin_change_problem.js
+++ coin_change_problem.js
@@ -8,18 +8,16 @@
 // 4. 2¢, 2¢
 
 function changePossibilitiesTopDown(amountLeft, denominations, currentIndex) {
-  if (amountLeft === 0) {
-    return 1;
-  }
+  if(amountLeft === 0) return 1;
 
-  if (amountLeft < 0) {
+  if(amountLeft < 0 ) {
     return 0;
   }
 
   let nCombos = 0;
 
-  for (let i = currentIndex; i < denominations.length; i++) {
-    nCombos = nCombos + changePossibilitiesTopDown(amountLeft - denominations[i], denominations, i)
+  for(let i = currentIndex; i < denominations.length; i++) {
+    nCombos += changePossibilitiesTopDown(amountLeft - denominations[i], denominations, i);
   }
   return nCombos;
 }
